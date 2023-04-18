dd1c7bbb3bba64c9691a8f845991cccd6a0befcc
Supakrit65 <supakritaphonmaeklong@gmail.com>
2023-04-18 22:56:34 +0700

    back

--- src/game/ClydeAI.java
+++ src/game/ClydeAI.java
@@ -4,24 +4,12 @@ import entity.Entity;
 import java.util.Random;
 
 public class ClydeAI implements AI{
-    private int currentRandomIndex;
-    private final int targetChangeInterval;
-    private int updateCounter;
-
-    public ClydeAI() {
-        currentRandomIndex = 0;
-        targetChangeInterval=2;
-        updateCounter = 0;
-    }
 
     @Override
     public String getNextMove(Entity ghost, Entity pacman, Game game){
-        String[] direction = {"N", "E", "W", "S"};
         Random rand = new Random();
-        if (updateCounter % targetChangeInterval == 0) {
-            currentRandomIndex = rand.nextInt(direction.length);
-        }
-        updateCounter++;
-        return direction[currentRandomIndex];
+        String[] direction = {"N", "E", "W", "S"};
+        int randomIndex = rand.nextInt(direction.length);
+        return direction[randomIndex];
     }
 }
